116400073132fcffb6676e3456b7f6ad6ed0b949
brandonaray <brandonray1225@gmail.com>
2019-05-10 15:58:50 -0500

    add create action and view for accounts

--- app/controllers/api/accounts_controller.rb
+++ app/controllers/api/accounts_controller.rb
@@ -1,2 +1,15 @@
 class Api::AccountsController < ApplicationController
+
+  def create
+    @account = Account.new(
+      credit_limit: params[:credit_limit],
+      apr: params[:apr]
+    )
+    if @account.save
+      render "show.json.jbuilder"
+    else
+      render json: {errors: @account.errors.full_messages}, status: 422
+    end
+  end
+
 end
---
+++ app/views/api/accounts/show.json.jbuilder
@@ -0,0 +1,3 @@
+json.id account.id
+json.credit_limit account.credit_limit
+json.apr account.apr
