4a0cc82236016d9883599497d25a9cc47dd7c664
brandonaray <brandonray1225@gmail.com>
2019-05-12 14:31:53 -0500

    add validations to models

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,5 @@
 class Account < ApplicationRecord
+  has_many :transactions
+  validates :credit_limit, numericality: {greater_than_or_equal_to: 0}
+  validates :apr, numericality: {greater_than_or_equal_to: 0}
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,5 @@
 class Transaction < ApplicationRecord
+  belongs_to :account
+  validates :amount, numericality: true
+  validates :account_id, numericality: {only_integer: true}
 end
