a69a9800fffc78e9ce3153724a396c500a9284db
brandonaray <brandonray1225@gmail.com>
2019-05-12 14:41:31 -0500

    add index and create actions to transactions

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -1,2 +1,21 @@
 class Api::TransactionsController < ApplicationController
+
+  def index
+    @transactions = Transaction.where(account_id: params[:account_id])
+    render "index.json.jbuilder"
+  end
+
+  def create
+    @transaction = Transaction.new(
+      type: params[:type],
+      amount: params[:amount],
+      account_id: params[:account_id]
+    )
+    if @transaction.save
+      render "show.json.jbuilder"
+    else
+      render json: {errors: @transaction.errors.full_messages}, status: 422
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     post "/accounts" => "accounts#create"
     get "/accounts/:id" => "accounts#show"
+
+    post "/transactions" => "transactions#create"
   end
 end
